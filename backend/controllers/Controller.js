e01787652b119e98f331001498f759703504814e
Yamani Yuda <yamaniyuda@gmail.com>
2022-03-08 15:03:34 +0700

    dd

--- backend/controllers/Controller.js
+++ backend/controllers/Controller.js
@@ -29,8 +29,6 @@ class Controller {
     try {
       form.parse(req, (err, fields, files) => {
         if (err) throw err
-        console.log(files)
-        console.log(fields)
         
         if (files.image === undefined)
           res.status(422).json({ msg: 'connot be proceed' }).end()
@@ -47,8 +45,8 @@ class Controller {
 
           fields.image = fileName
           this.model().insertData(fields, (data) => {
-            if (data > 0) throw new Error('cannot insert')
-            res.status(200).json({ msg: 'new added successfully' }).end()
+            if (data == 0) throw new Error('cannot insert')
+            res.status(200).json({ msg : 'new added successfully' }).end()
           })
         })
         res.json(files)
@@ -62,25 +60,26 @@ class Controller {
     try {
       form.parse(req, (err, fields, files) => {
         if (err) throw err
-        if (files.image !== undefined) {
-          const fileName = files.image.newFilename + '.' + files.image.mimetype.split('/')[1]
-          const pathNow = files.image.filepath + '.' + files.image.mimetype.split('/')[1]
+        if (files.image) {
+          const ext = this.checkExtensi(files.image.mimetype, ['jpg', 'png', 'jpeg'])
+          if (ext === false) throw new Error('failed file')
+          
+          const fileName = files.image.newFilename + '.' + ext
+          const pathNow = files.image.filepath
           pathSave = pathSave + fileName
 
-          fs.rename(pathNow, pathSave, (err) => {
+          mv(pathNow, pathSave, (err) => {
             if (err) throw err
 
             fields.image = fileName
-            this.model().insertData(fields, (data) => {
-              if (data > 0) throw new Error('cannot insert')
-              res.status(200).json({ msg: `${this.database} updated successfully` })
+            const compared = { id: 'id', value: fields.id }
+            this.model().updateData(fields, compared, (change) => {
+              if (change == 0) throw new Error('cannot updateData')
+              res.status(200).json({ msg: 'updated successfully' }).end()
             })
           })
         }
-        this.model().insertData(fields, (data) => {
-          if (data > 0) throw new Error('cannot insert')
-          res.status(200).json({ msg: `${this.database} updated successfully` })
-        })
+
       })
     } catch (err) {
       res.status(422).json({ msg: 'connot be proceed' })
